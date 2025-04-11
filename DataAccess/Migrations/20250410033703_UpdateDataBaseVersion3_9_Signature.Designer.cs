7e9874ee1f7124820e07e4eeea156c41ac9af772
AmbroseSu <ambrose472263@gmail.com>
2025-04-11 15:54:02 +0700

    fix_db_digital_signature

--- DataAccess/Migrations/20250410033703_UpdateDataBaseVersion3_9_Signature.Designer.cs
+++ DataAccess/Migrations/20250410033703_UpdateDataBaseVersion3_9_Signature.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(DocumentManagementSystemDbContext))]
-    partial class DocumentManagementSystemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250410033703_UpdateDataBaseVersion3_9_Signature")]
+    partial class UpdateDataBaseVersion3_9_Signature
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DataAccess/Migrations/20250410033703_UpdateDataBaseVersion3_9_Signature.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDataBaseVersion3_9_Signature : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SignatureValue",
+                table: "DocumentSignature");
+
+            migrationBuilder.DropColumn(
+                name: "ValidFrom",
+                table: "DocumentSignature");
+
+            migrationBuilder.DropColumn(
+                name: "HashAlgorithm",
+                table: "DigitalCertificate");
+
+            migrationBuilder.DropColumn(
+                name: "IsRevoked",
+                table: "DigitalCertificate");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerName",
+                table: "DigitalCertificate");
+
+            migrationBuilder.DropColumn(
+                name: "SignatureValue",
+                table: "ArchiveDocumentSignature");
+
+            migrationBuilder.DropColumn(
+                name: "ValidFrom",
+                table: "ArchiveDocumentSignature");
+
+            migrationBuilder.RenameColumn(
+                name: "PublicKey",
+                table: "DigitalCertificate",
+                newName: "Subject");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Subject",
+                table: "DigitalCertificate",
+                newName: "PublicKey");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SignatureValue",
+                table: "DocumentSignature",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ValidFrom",
+                table: "DocumentSignature",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "HashAlgorithm",
+                table: "DigitalCertificate",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRevoked",
+                table: "DigitalCertificate",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerName",
+                table: "DigitalCertificate",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SignatureValue",
+                table: "ArchiveDocumentSignature",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ValidFrom",
+                table: "ArchiveDocumentSignature",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
