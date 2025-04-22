ec1e2de1fefc3443cdd4af140f0814a5ee6ea756
Wiramin <113937901+cloudcode366@users.noreply.github.com>
2025-04-22 10:26:25 +0700

    fix scope get all archive document

--- Service/Impl/ArchiveDocumentService.cs
+++ Service/Impl/ArchiveDocumentService.cs
@@ -7,6 +7,7 @@ using AutoMapper;
 using BusinessObject;
 using DataAccess.DTO;
 using DataAccess.DTO.Response;
+using DocumentFormat.OpenXml.Office.CustomXsn;
 using Org.BouncyCastle.X509;
 using Org.BouncyCastle.Security;
 using iText.Signatures;
@@ -123,7 +124,7 @@ public partial class ArchiveDocumentService : IArchiveDocumentService
                 x.NumberOfDocument,
                 x.CreatedDate,
                 x.CreatedBy,
-                x.Scope,
+                Scope = x.Scope.ToString(),
                 x.Sender,
                 x.ExternalPartner,
                 x.DateReceived,
