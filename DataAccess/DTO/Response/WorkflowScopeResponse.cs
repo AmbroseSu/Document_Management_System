92363343165bf9bfe84ca01bc7a0fe54bfeba17d
AmbroseSu <ambrose472263@gmail.com>
2025-05-21 14:56:30 +0700

    fix_workflow_scope

--- DataAccess/DTO/Response/WorkflowScopeResponse.cs
+++ DataAccess/DTO/Response/WorkflowScopeResponse.cs
@@ -4,5 +4,6 @@ public class WorkflowScopeResponse
 {
     public Guid WorkflowId { get; set; }
     public String WorkflowName { get; set; }
+    public String FirstRole { get; set; }
     public List<DocumentTypeDto> DocumentTypes { get; set; }
 }
