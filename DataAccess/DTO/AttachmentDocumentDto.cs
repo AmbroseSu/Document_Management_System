056b142dc78ede05fdf1b741a4d04f563d7699c6
AmbroseSu <ambrose472263@gmail.com>
2025-05-29 13:55:57 +0700

    add_attachment_doc_response

--- DataAccess/DTO/AttachmentDocumentDto.cs
+++ DataAccess/DTO/AttachmentDocumentDto.cs
@@ -5,8 +5,5 @@ public class AttachmentDocumentDto
     public Guid AttachmentDocumentId { get; set; }
     public string? AttachmentDocumentName { get; set; }
     public string? AttachmentDocumentUrl { get; set; }
-    public bool IsDeleted { get; set; }
 
-    public Guid? DocumentId { get; set; }
-    public Guid? DocumentFileExtensionId { get; set; }
 }
